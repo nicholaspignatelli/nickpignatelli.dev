8ae88b4f4a229182e8675132b73d390e3f851bf3
Nicholas Pignatelli <nickpignatelli@icloud.com>
2023-01-10 10:48:04 -0800

    impl styles for semantic childeen

---
+++ src/components/ContactForm/styled/StyledField.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { Field } from "formik";
+
+const StyledField = styled(Field)`
+  border: 0.1rem solid var(--prime-color);
+  display: flex;
+  align-items: center;
+`;
+
+export default StyledField;
