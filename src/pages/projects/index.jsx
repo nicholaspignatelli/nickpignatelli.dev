aebf2b5010da5a185e77fe265b9c434d9a8664bc
Nicholas Pignatelli <nickpignatelli@icloud.com>
2023-01-10 11:01:58 -0800

    impl Projects

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,5 +1,23 @@
-const Projects = () => {
-  return Projects;
+import styled from "styled-components";
+import StyledGridItem from "../../components/GridItem/styled/StyledGridItem";
+import ProjectCard from "../../components/ProjectCard";
+import Title from "../../styled/Title.styled";
+
+const Projects = ({ projects }) => {
+  return (
+    <StyledGridItem gridArea="projects" id={"Projects"}>
+      <div>
+        <Title>Projects</Title>
+        {projects.map((proj) => (
+          <ProjectCard
+            name={proj.name}
+            link={proj.link}
+            description={proj.description}
+          />
+        ))}
+      </div>
+    </StyledGridItem>
+  );
 };
 
 export default Projects;
