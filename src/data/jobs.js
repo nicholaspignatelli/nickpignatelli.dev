804e6dd9b071e674ab69909908a7765ee26f9516
Nicholas Pignatelli <nickpignatelli@icloud.com>
2023-01-10 10:33:37 -0800

    content/hydration for experience page

---
+++ src/data/jobs.js
@@ -0,0 +1,57 @@
+const jobs = [
+  {
+    title: "Software Engineer",
+    company: "HSBC",
+    description: [
+      "Leveraged React.js to build a modern component-driven UI for a new customer experience.",
+      "Introduced performance optimizations that brought application performance up by reducing unnecessary rerenders.",
+    ],
+    durationArray: ["Mar 2022", "Dec 2022"],
+    techStack: ["JavaScript", "React", "node.js", "git", "JIRA", "HTTP"],
+  },
+  {
+    title: "Software Engineer I",
+    company: "Scotiabank",
+    description: [
+      "Delivered Track & Trace API that exposed end-to-end payment tracking for global business customers, leveraging Java, Spring, Mulesoft, Swagger, and other Java-based tools.",
+      "Championed effective conduct in agile ceremonies, increasing team velocity rate by 50% through applying feedback retrospectives, and scrums.",
+    ],
+
+    durationArray: ["Jun 2021", "Mar 2022"],
+    techStack: ["Java", "Spring", "Swagger.io", "gradle", "JUnit", "log4j"],
+  },
+  {
+    title: "Public Cloud Administrator (Co-op)",
+    company: "Scotiabank",
+    description: [
+      "Facilitated multi-user cloud computing environments by applying automated security patches to vulnerable VMs.",
+      "Efficiently managed AWS & Azure cloud resources, ensuring minimal costs and no wastage.",
+    ],
+    durationArray: ["May 2020", "Aug 2020"],
+    techStack: ["Microsoft Azure", "SALT Stack", "powershell"],
+  },
+  {
+    title: "Software Engineer (Co-op)",
+    company: "Scotiabank",
+    description: [
+      "Built a fault-tolerant, asynchronous detection engine on the JVM, leveraging Kotlin, Kafka, ElasticSearch, increasing trading floor productivity by 15%.",
+      "Automated the monitoring of event stream data, providing key trading floor executives real time feedback about the state of the infrastructure. Used PagerDuty for real time notifications.",
+    ],
+    durationArray: ["May 2019", "Summer 2019"],
+
+    techStack: ["Kotlin", "Vert.x", "ElasticSearch", "Kafka"],
+  },
+  {
+    title: "Junior Programmer Analyst (Co-op)",
+    company: "Scotiabank",
+    description: [
+      "Using a message-oriented integration pattern, built RESTful APIs to facilitate an ETL workflow for a payment modernization initiative. Used Java, IBM WebSphere MQ.",
+      "Safely and successfully migrated data for 3000+ businesses with zero customer impact and 100% uptime.",
+    ],
+    durationArray: ["Sept 2018", "December 2018"],
+
+    techStack: ["Java", "MySQL", "FTP"],
+  },
+];
+
+export default jobs;
