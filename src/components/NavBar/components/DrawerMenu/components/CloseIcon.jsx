91a16d6633ed7fec294ba11327ee94e4ff2ada9c
Nicholas Pignatelli <nickpignatelli@icloud.com>
2023-01-10 10:41:47 -0800

    impl svg component for drawer menu

---
+++ src/components/NavBar/components/DrawerMenu/components/CloseIcon.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const CloseIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2 2L18 18"
+        stroke="#fff"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M2 18L18 2"
+        stroke="#fff"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default CloseIcon;
