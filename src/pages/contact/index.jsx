761cc214fd9fe5c5772b6052aa13395d8ffc138a
Nicholas Pignatelli <nickpignatelli@icloud.com>
2023-01-10 11:00:34 -0800

    impl Contact

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,5 +1,14 @@
-const Contact = () => {
-  return Contact;
-};
+import StyledGridItem from "../../components/GridItem/styled/StyledGridItem";
+import ContactForm from "../../components/ContactForm";
+import Title from "../../styled/Title.styled";
+
+const Contact = () => (
+  <StyledGridItem gridArea="contact" id="Contact">
+    <div>
+      <Title>Contact</Title>
+      <ContactForm />
+    </div>
+  </StyledGridItem>
+);
 
 export default Contact;
