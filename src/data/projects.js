39f2e2fc67eeb743756f05347d6c2ef8921ebbb9
Nicholas Pignatelli <nickpignatelli@icloud.com>
2023-01-10 10:33:56 -0800

    content/hydration for projects page

---
+++ src/data/projects.js
@@ -0,0 +1,19 @@
+const projects = [
+  {
+    name: "virtual-keyboard",
+    link: "https://github.com/nicholaspignatelli/virtual-keyboard",
+    description: "A simple qwerty keyboard rendered in the dom.",
+  },
+  {
+    name: "helpings",
+    link: "https://github.com/nicholaspignatelli/",
+    description: "A MERN stack app to help me stay organized in the kitchen!",
+  },
+  {
+    name: "babel",
+    link: "https://github.com/nicholaspignatelli/",
+    description: "lorem ipsum dolor your mom",
+  },
+];
+
+export default projects;
