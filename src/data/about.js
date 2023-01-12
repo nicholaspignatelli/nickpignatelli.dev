064d92793df597115d875ac2ebf2377fb82c4c8b
Nicholas Pignatelli <nickpignatelli@icloud.com>
2023-01-12 12:25:08 -0800

    add content object for hydration

---
+++ src/data/about.js
@@ -0,0 +1,75 @@
+const about = {
+  skills: [
+    {
+      name: "HTML",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg",
+    },
+    {
+      name: "CSS",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+    },
+    {
+      name: "JavaScript",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg",
+    },
+    {
+      name: "Java",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/d/dd/Java_programming_language_logo.png",
+    },
+    {
+      name: "AWS",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg",
+    },
+    {
+      name: "node.js",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg",
+    },
+    {
+      name: "Postman",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/c/c2/Postman_%28software%29.png",
+    },
+    {
+      name: "express.js",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png",
+    },
+    {
+      name: "Mongo DB",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg",
+    },
+    {
+      name: "git",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg",
+    },
+    {
+      name: "JIRA",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/8/82/Jira_%28Software%29_logo.svg",
+    },
+    {
+      name: "Kafka",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/0/05/Apache_kafka.svg",
+    },
+    {
+      name: "React",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+    },
+    {
+      name: "Spring",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/4/44/Spring_Framework_Logo_2018.svg",
+    },
+  ],
+};
+export default about;
