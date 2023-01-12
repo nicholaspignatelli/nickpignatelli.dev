434587fd7dbb6c3c5d78fa709f1720e10261fef9
Nicholas Pignatelli <nickpignatelli@icloud.com>
2023-01-12 12:21:39 -0800

    add padding for form inputs

--- src/components/ContactForm/styled/StyledField.js
+++ src/components/ContactForm/styled/StyledField.js
@@ -2,9 +2,10 @@ import styled from "styled-components";
 import { Field } from "formik";
 
 const StyledField = styled(Field)`
-  border: 0.1rem solid var(--prime-color);
+  border: 0.1rem solid var(--prime-color-a50);
   display: flex;
   align-items: center;
+  padding: 0.2rem 0.3rem 0;
 `;
 
 export default StyledField;
